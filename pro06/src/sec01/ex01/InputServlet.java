9548c43286b197a36ae488ac93ebceb7af3729b9
zooxop <zooxop@gmail.com>
2021-01-10 19:46:56 +0900

    learning use servlet

---
+++ pro06/src/sec01/ex01/InputServlet.java
@@ -0,0 +1,51 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class InputServlet
+ */
+@WebServlet("/input")
+public class InputServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("Request method 'init'");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		System.out.println("Request method 'destroy'");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		
+		String user_id = request.getParameter("user_id");
+		String user_pw = request.getParameter("user_pw");
+		
+		System.out.println("ID :" + user_id);
+		System.out.println("PW :" + user_pw);
+		
+		String[] subject = request.getParameterValues("subject");
+		for(String str : subject) {
+			System.out.println("Selected subject : " + str);
+		}
+	}
+
+}
