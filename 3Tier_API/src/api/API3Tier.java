ca6e008928ea31d803aa6664b0c0159cec356945
zooxop <zooxop@gmail.com>
2021-01-31 23:42:35 +0900

    learned difference of abstract class and interface

--- 3Tier_API/src/api/API3Tier.java
+++ 3Tier_API/src/api/API3Tier.java
@@ -1,9 +1,14 @@
 package api;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 //model 개발 시 상속시킬 추상 클래스
 public abstract class API3Tier {
 	String DataType;  //xml, json 등 구분.
-	
+	DataSource dataFactory;
+
 	public String getDataType() {
 		return DataType;
 	}
@@ -11,7 +16,19 @@ public abstract class API3Tier {
 		DataType = dataType;
 	}
 	
-	public abstract void dbConn();  //각 병원별, DB종류별 connection. (use ConnectionPool)
+	public DataSource dbConn() {
+		//DB종류(oracle, tibero) 상관없이 connection. (use ConnectionPool)
+		
+		try {
+			Context ctx = new InitialContext();
+			Context envContext = (Context) ctx.lookup("java:/comp/env");
+			dataFactory = (DataSource) envContext.lookup("jdbc/oracle");  //oracle 명칭은 바꾸기만 하면 됨.
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+		return dataFactory;
+	}
+	
 	public abstract void getRequest();  //xml, json 등 요청 데이터 받아오기
 	public abstract void inputDataParse();  //xml, json 등 요청 데이터 파싱 
 	public abstract void outputDataParse(); //xml, json 등 리턴 데이터 파싱
--- 3Tier_API/src/api/model/ApiModel.java
+++ 3Tier_API/src/api/model/ApiModel.java
@@ -1,35 +1,34 @@
 package api.model;
 
+import javax.sql.DataSource;
+
 import api.API3Tier;
 
 public abstract class ApiModel extends API3Tier{
 
 	@Override
-	public void dbConn() {
+	public void getRequest() {  //이건 추상이 맞다.
+		
+		DataSource dataFactory = dbConn();  //이런식으로 디비 연결?
+
+		
+		
+	}
+
+	@Override
+	public void inputDataParse() {  //이건 xml, json, ADO ResultSet 에 따라 달라질 수 있으니 Interface?
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void getRequest() {
+	public void outputDataParse() {  //이건 xml, json, ADO ResultSet 에 따라 달라질 수 있으니 Interface?
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void inputDataParse() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void outputDataParse() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void sendResponse() {
+	public void sendResponse() {  //view로 전송하는 단계이니 추상클래스?
 		// TODO Auto-generated method stub
 		
 	}
