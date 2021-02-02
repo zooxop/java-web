201cf5c0c213c05611d3b0a2b87ed32413532a96
zooxop <zooxop@gmail.com>
2021-02-03 00:43:10 +0900

    just coding

--- 3Tier_API/src/api/controller/ApiController.java
+++ 3Tier_API/src/api/controller/ApiController.java
@@ -20,10 +20,12 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
+import api.model.ApiModel;
 
 
 
-@WebServlet("/NurionAPI_001.jsp")
+
+@WebServlet("/NurionAPI_KUMC_AA.jsp")
 public class ApiController {
 	
 	public void init(ServletConfig config) throws ServletException {
@@ -54,66 +56,13 @@ public class ApiController {
 		try {
 			String strXml = request.getParameter("strXml");
 			
-			HashMap<String, Object> htData = new HashMap<String, Object>();
-			
 			StringReader sr = new StringReader(strXml);
 			
 			//이 아래로 전부다 인터페이스로 넘기자
-			InputSource is = new InputSource(sr);
 			
+			ApiModel apiModel = new ApiModel(sr, request, response);
 			
-			
-			DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-			Document doc = docBuilder.parse(is);
-			doc.getDocumentElement().normalize();  //xml정규화
-			XPath xpath = XPathFactory.newInstance().newXPath();
-			
-			Element root = doc.getDocumentElement();
-
-			String dataType = checkValidParam("dataType", root, doc);
-			String count = checkValidParam("count", root, doc);
-			String schemaCode = checkValidParam("schemaCode", root, doc);
-			
-			int cnt = Integer.parseInt(count);
-			
-			String tagName = "";
-			Node nodeData = null;
-			NodeList nodeList = null;
-			Node node = null;
-			
-			String strNodeName = "";
-			String strNodeValue = "";
-			String strMethodName = "";
-			
-			htData.put("dataType", dataType);
-			htData.put("count", count);
-			htData.put("schemaCode", schemaCode);
-			
-			for (int i = 0; i < cnt; i++) {
-				nodeData = doc.getElementsByTagName("data_" + (i+1)).item(0);
-				nodeList = nodeData.getChildNodes();
-				
-				HashMap<String, String> htMethod  = new HashMap<String, String>();
-				
-				for (int j = 0; j < nodeList.getLength(); j++) {
-					node = nodeList.item(j);
-					
-					if(node.getNodeType() == Node.ELEMENT_NODE) {
-						
-						strNodeName = node.getNodeName();
-						strNodeName = strNodeName.toUpperCase();
-						
-						strNodeValue = node.getTextContent();
-						htMethod.put(strNodeName, strNodeValue);
-						
-						if (strNodeName.equals("METHODNAME")) {
-							strMethodName = strNodeValue;
-						}
-					}
-				}
-				
-				request.setAttribute("htMethod_" + (i+1), htMethod);
-			}
+			apiModel.getRequest();  //여기서 리턴 받는걸 view 로 넘기자.
 			
 		} catch (Exception e) {
 			
@@ -126,22 +75,5 @@ public class ApiController {
 	}
 	
 	
-	// 파라메터 유효성 검사.
-	private String checkValidParam(String item, Element root, Document document) {
-		String TempStr = "";
-		NodeList childSchema = root.getChildNodes();
-		for (int i = 0; i < childSchema.getLength(); i++){
-			Node nodeSchema = childSchema.item(i);
-
-			if (nodeSchema.getNodeType() == Node.ELEMENT_NODE){
-				Element elementSchema = (Element)nodeSchema;
-
-				if (elementSchema.getNodeName().equals(item)){
-					TempStr = document.getElementsByTagName(item).item(0).getTextContent();
-					break;
-				}
-			}
-		}
-		return TempStr;
-	}
+	
 }
--- 3Tier_API/src/api/model/ApiModel.java
+++ 3Tier_API/src/api/model/ApiModel.java
@@ -1,17 +1,118 @@
 package api.model;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 
 import api.API3Tier;
 
-public abstract class ApiModel extends API3Tier{
+public class ApiModel extends API3Tier{
+	
+	private StringReader sr;
+	private HttpServletRequest request;
+	private HttpServletResponse response;
 
+	public void setRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+
+	public HttpServletResponse getResponse() {
+		return response;
+	}
+
+	public void setResponse(HttpServletResponse response) {
+		this.response = response;
+	}
+
+	public void setSr(StringReader sr) {
+		this.sr = sr;
+	}
+
+	public ApiModel(StringReader stringReader, HttpServletRequest pRequest, HttpServletResponse pResponse) throws ServletException, IOException {
+		setSr(stringReader);
+		setRequest(request);
+		setResponse(response);
+	}
+	
 	@Override
 	public void getRequest() {  //이건 추상이 맞다.
 		
 		DataSource dataFactory = dbConn();  //이런식으로 디비 연결?
-
 		
+		HashMap<String, Object> htData = new HashMap<String, Object>();
+		InputSource is = new InputSource(sr);
+	
+		try {
+			DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			
+			Document doc = docBuilder.parse(is);
+			doc.getDocumentElement().normalize();  //xml정규화
+			XPath xpath = XPathFactory.newInstance().newXPath();
+			
+			Element root = doc.getDocumentElement();
+
+			String dataType = checkValidParam("dataType", root, doc);
+			String count = checkValidParam("count", root, doc);
+			String schemaCode = checkValidParam("schemaCode", root, doc);
+			
+			int cnt = Integer.parseInt(count);
+			
+			String tagName = "";
+			Node nodeData = null;
+			NodeList nodeList = null;
+			Node node = null;
+			
+			String strNodeName = "";
+			String strNodeValue = "";
+			String strMethodName = "";
+			
+			htData.put("dataType", dataType);
+			htData.put("count", count);
+			htData.put("schemaCode", schemaCode);
+			
+			for (int i = 0; i < cnt; i++) {
+				nodeData = doc.getElementsByTagName("data_" + (i+1)).item(0);
+				nodeList = nodeData.getChildNodes();
+				
+				HashMap<String, String> htMethod  = new HashMap<String, String>();
+				
+				for (int j = 0; j < nodeList.getLength(); j++) {
+					node = nodeList.item(j);
+					
+					if(node.getNodeType() == Node.ELEMENT_NODE) {
+						
+						strNodeName = node.getNodeName();
+						strNodeName = strNodeName.toUpperCase();
+						
+						strNodeValue = node.getTextContent();
+						htMethod.put(strNodeName, strNodeValue);
+						
+						if (strNodeName.equals("METHODNAME")) {
+							strMethodName = strNodeValue;
+						}
+					}
+				}
+				
+				request.setAttribute("htMethod_" + (i+1), htMethod);
+			}
+		} catch (Exception e) {
+			e.getStackTrace();
+		}
 		
 	}
 
@@ -31,6 +132,25 @@ public abstract class ApiModel extends API3Tier{
 	public void sendResponse() {  //view로 전송하는 단계이니 추상클래스?
 		// TODO Auto-generated method stub
 		
+	}
+	
+	// 파라메터 유효성 검사.
+	private String checkValidParam(String item, Element root, Document document) {
+		String TempStr = "";
+		NodeList childSchema = root.getChildNodes();
+		for (int i = 0; i < childSchema.getLength(); i++){
+			Node nodeSchema = childSchema.item(i);
+
+			if (nodeSchema.getNodeType() == Node.ELEMENT_NODE){
+				Element elementSchema = (Element)nodeSchema;
+
+				if (elementSchema.getNodeName().equals(item)){
+					TempStr = document.getElementsByTagName(item).item(0).getTextContent();
+					break;
+				}
+			}
+		}
+		return TempStr;
 	}
 
 }
